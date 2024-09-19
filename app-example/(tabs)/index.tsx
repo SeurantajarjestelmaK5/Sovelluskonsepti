ac170182fd75b66a1fb86ddde0340128b1e3abb3
Serveriii <n2jose00@students.oamk.fi>
2024-09-19 11:01:43 +0300

    initial commit

--- app-example/(tabs)/index.tsx
+++ app-example/(tabs)/index.tsx
@@ -8,7 +8,7 @@ import { ThemedView } from '@/components/ThemedView';
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: '#ce11c4', dark: '#a71293' }}
       headerImage={
         <Image
           source={require('@/assets/images/partial-react-logo.png')}
