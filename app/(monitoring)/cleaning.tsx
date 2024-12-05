2b467182c102fb6db68464f66a14ba18829912b1
Serveriii <n2jose00@students.oamk.fi>
2024-12-05 17:13:18 +0200

    Working weekcalendar forntend + expo update

--- app/(monitoring)/cleaning.tsx
+++ app/(monitoring)/cleaning.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { Link } from "expo-router";
 import { Pressable, Text, View } from "react-native";
 import { Button } from "react-native-paper";
@@ -7,25 +7,81 @@ import AntDesign from "@expo/vector-icons/AntDesign";
 
 import { useThemeColors } from "@/constants/ThemeColors";
 import { getCleaningStyles } from "@/styles/monitoring/cleaningStyles";
-import WeekView from "@/components/WeekView";
 
-export default function cleaning() {
-  const [week, setWeek] = useState(1);
+export default function Cleaning() {
+  const [year, setYear] = useState<string>("");
+  const [startOfWeek, setStartOfWeek] = useState<string>("");
+  const [endOfWeek, setEndOfWeek] = useState<string>("");
+  const [currentDate, setCurrentDate] = useState<Date>(new Date()); 
   const [selectedSide, setSelectedSide] = useState("Keittiö");
 
   const ThemeColors = useThemeColors();
   const styles = useMemo(() => getCleaningStyles(ThemeColors), [ThemeColors]);
 
+  useEffect(() => {
+    updateWeekRange(currentDate); 
+  }, [currentDate]);
+
+  function updateWeekRange(date: Date) {
+    const { startOfWeek, endOfWeek } = getWeekRange(date);
+
+    // Formatter function
+    const formatDate = (date: Date) => {
+      const day = date.getDate();
+      const month = date.getMonth() + 1; 
+      return `${day}.${month}.`;
+    };
+
+    // Format dates and update state
+    setYear(startOfWeek.getFullYear().toString());
+    setStartOfWeek(formatDate(startOfWeek));
+    setEndOfWeek(formatDate(endOfWeek));
+  }
+
+  function getWeekRange(date: Date) {
+    const startOfWeek = new Date(date);
+    startOfWeek.setDate(date.getDate() - date.getDay() + 1); 
+    const endOfWeek = new Date(startOfWeek);
+    endOfWeek.setDate(startOfWeek.getDate() + 6); 
+    return { startOfWeek, endOfWeek };
+  }
+
+  function handleWeekChange(offset: number) {
+    const newDate = new Date(currentDate);
+    newDate.setDate(currentDate.getDate() + offset);
+    setCurrentDate(newDate);
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.content}>
         <Text style={styles.header}>Viikkosiivous</Text>
-        {/* <View style={styles.weekCalendar}>
-          <AntDesign name="caretleft" size={24} color={ThemeColors.tint} />
-          <Text style={styles.text}>Viikko 1 6. - 12.1.2025 </Text>
-          <AntDesign name="caretright" size={24} color={ThemeColors.tint} />
-        </View> */}
-        <WeekView />
+        <View style={styles.weekCalendar}>
+          <Pressable
+            onPress={() => handleWeekChange(-7)}
+            hitSlop={{ top: 10, right: 10, bottom: 10, left: 10 }}
+          >
+            <AntDesign
+              name="caretleft"
+              size={24}
+              color={ThemeColors.tint}
+            />
+          </Pressable>
+          <Text style={styles.text}>
+            {startOfWeek} - {endOfWeek} {year}
+          </Text>
+          <Pressable
+            onPress={() => handleWeekChange(7)}
+            hitSlop={{ top: 10, right: 10, bottom: 10, left: 10 }}
+          >
+            <AntDesign
+              name="caretright"
+              size={24}
+              color={ThemeColors.tint}
+            />
+          </Pressable>
+        </View>
+
         <View style={styles.selectionContainer}>
           <Pressable
             onPress={() => {
