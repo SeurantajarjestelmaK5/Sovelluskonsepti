634f52dc2be5ef43460b57483b6c70261b7a42b5
Serveriii <n2jose00@students.oamk.fi>
2025-05-07 20:33:31 +0300

    Setup for meat temps

--- components/views/MeatTemperatures.tsx
+++ components/views/MeatTemperatures.tsx
@@ -1,18 +1,54 @@
 import React, { useEffect, useMemo, useState } from "react";
-import { Alert, FlatList, Text, View } from "react-native";
+import { Alert, FlatList, Text, View, TextInput } from "react-native";
 import { useThemeColors } from "@/constants/ThemeColors";
-import { getWashingTempStyles } from "@/styles/views/washingMachineTempStyle";
+import { getMeatTempStyles } from "@/styles/views/meatTempStyle";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Button } from "react-native-paper";
 
 export default function WashingMachineTemperatures() {
   const ThemeColors = useThemeColors();
-  const styles = getWashingTempStyles(ThemeColors);
+  const styles = getMeatTempStyles(ThemeColors);
+  const [beefTemp, setBeefTemp] = useState(0);
+  const [porkTemp, setPorkTemp] = useState(0);
 
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 24, color: "white" }}>Remontissa!</Text>
-      <Text style={{ fontSize: 24, color: "white" }}>Palaa myöhemmin.</Text>
+      <View style={styles.measurement}>
+        <Text style={styles.header}>Nauta</Text>
+        <View style={styles.inputContainer}>
+          <Text style={styles.header}>Lämpötila:</Text>
+          <TextInput
+            style={styles.input}
+            value={beefTemp.toString()}
+            keyboardType="numeric"
+            onChangeText={(text) => {
+              const value = parseFloat(text);
+              if (!isNaN(value)) {
+                setBeefTemp(value);
+              } else {
+                Alert.alert("Virheellinen syöte", "Syötä kelvollinen luku.");
+              }
+            }}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.header}>Päivämäärä:
+          </Text>
+          <TextInput
+            style={styles.input}
+            value={beefTemp.toString()}
+            keyboardType="numeric"
+            onChangeText={(text) => {
+              const value = parseFloat(text);
+              if (!isNaN(value)) {
+                setBeefTemp(value);
+              } else {
+                Alert.alert("Virheellinen syöte", "Syötä kelvollinen luku.");
+              }
+            }}
+          />
+        </View>
+      </View>
     </View>
   );
 }
--- styles/views/meatTempStyle.ts
+++ styles/views/meatTempStyle.ts
@@ -1,21 +1,47 @@
-import {StyleSheet} from "react-native";
+import { StyleSheet } from "react-native";
 import { ThemeColors } from "@/constants/ThemeColors";
 
 export const getMeatTempStyles = (ThemeColors: ThemeColors) =>
-    StyleSheet.create({
-        container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 10,
-        backgroundColor: ThemeColors.background,
-        },
-        content: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 16,
-        backgroundColor: ThemeColors.background,
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "flex-start",
+      alignItems: "center",
+      padding: 10,
+      backgroundColor: ThemeColors.background,
+    },
+    content: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      padding: 16,
+      backgroundColor: ThemeColors.background,
+      width: "100%",
+    },
+    header: {
+      fontSize: 20,
+      color: "white",
+      marginBottom: 15,
+    },
+    input: {
+        height: 40,
         width: "100%",
-        },
-    });
+        borderRadius: 5,
+        paddingHorizontal: 10,
+        color: 'black',
+        backgroundColor: 'white',
+        textAlign: "center",
+    },
+    inputContainer: {
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    measurement: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      width: "100%",
+      padding: 10,
+    },
+  });
